bc668b030fcb95b55086d873920f402d45db697f
SiryjVyiko <you@example.com>
2015-10-28 20:45:56 +0200

    just an improvement

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for ROOT project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
-import os
+import os,sys
+virtenv = os.path.expanduser('~') + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    if sys.version.split(' ')[0].split('.')[0] == '3':
+        exec(compile(open(virtualenv, "rb").read(), virtualenv, 'exec'), dict(__file__=virtualenv))
+    else:
+        execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+  
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ROOT.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ROOT.settings")
-
 application = get_wsgi_application()
